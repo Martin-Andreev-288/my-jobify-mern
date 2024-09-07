63da2a534a3f54832748898b8aba5b7f1d0240f5
Martin Andreev <martin_i_a@abv.bg>
2024-09-07 19:29:40 +0300

    Connect to the mongoDB

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
-
+import mongoose from 'mongoose';
 // routers
 import jobRouter from './routes/jobRouter.js'; // we name it different
 
@@ -34,7 +34,12 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5100;
 
-app.listen(port, () => {
-    console.log(`server running on PORT ${port}....`);
-});
-
+try {
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(port, () => {
+        console.log(`server running on PORT ${port}....`);
+    });
+} catch (error) {
+    console.log(error);
+    process.exit(1);
+}
