f95314644f92a163084419328de7bcc60ec61de9
Martin Andreev <martin_i_a@abv.bg>
2024-09-09 16:50:54 +0300

    Setup validation for ID params creating a custom function
    Now if we have the same syntax but a wrong value for the ID, the error will be of type 404.
    If we have If we provide invalid syntax, then we get different type of error from the MongoDB (400).

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { validateJobInput } from '../middleware/validationMiddleware.js';
-
 const router = Router();
 
 import { getAllJobs, getJob, createJob, updateJob, deleteJob } from "../controllers/jobController.js";
+import { validateJobInput, validateIdParam } from '../middleware/validationMiddleware.js';
 
 router.route('/').get(getAllJobs).post(validateJobInput, createJob);
-router.route('/:id').get(getJob).patch(validateJobInput, updateJob).delete(deleteJob);
+router.route('/:id').get(validateIdParam, getJob).patch(validateJobInput, updateJob).delete(validateIdParam, deleteJob);
 
 export default router;
