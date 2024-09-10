9bf29aa6d435128b3cd7ee3476024729cdd03ed5
Martin Andreev <martin_i_a@abv.bg>
2024-09-10 17:08:43 +0300

    Create customFetch axios function and delete redundant test code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
-fetch("/api/v1/test")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+import axios from "axios";
+// Info for axios. THIS CODE HERE WILL BE DELETED AFTER THIS SCREENSHOT
+const resp = await axios.get("/api/v1/test");
+console.log(resp);
+// On the place of:
+// fetch("/api/v1/test")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
