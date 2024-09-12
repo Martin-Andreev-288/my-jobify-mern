6422a2c01810eb930db8299d671915f16befa43a
Martin Andreev <martin_i_a@abv.bg>
2024-09-13 01:24:22 +0300

    Add a change of the landing page description (no need for a new branch)

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -17,10 +17,9 @@ const Landing = () => {
             job <span>tracking</span> app
           </h1>
           <p>
-            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-            narwhal.
+            Track your job applications, view stats, and manage your profile
+            easily with My Jobify. Explore the app with sample data before
+            signing up. Perfect for staying organized in your job search.
           </p>
           <Link to="/register" className="btn register-link">
             Register
