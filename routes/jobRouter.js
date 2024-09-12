2d56167c51861d652c317ffa768e83828fa83bec
Martin Andreev <martin_i_a@abv.bg>
2024-09-12 16:24:17 +0300

    Starts to setup the stats route or the stats page. Group the stats by job status and by montly applications
    Format the date with dayjs extension.
    
    Sort the job applications from the last 6 months.

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-import { getAllJobs, getJob, createJob, updateJob, deleteJob } from "../controllers/jobController.js";
+import { getAllJobs, getJob, createJob, updateJob, deleteJob, showStats } from "../controllers/jobController.js";
 import { validateJobInput, validateIdParam } from '../middleware/validationMiddleware.js';
 import { checkForTestUser } from "../middleware/authMiddleware.js";
 
@@ -9,6 +9,9 @@ router
     .route('/')
     .get(getAllJobs)
     .post(checkForTestUser, validateJobInput, createJob);
+
+router.route('/stats').get(showStats);
+
 router
     .route('/:id')
     .get(validateIdParam, getJob)
