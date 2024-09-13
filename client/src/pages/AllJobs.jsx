0e1c635ad79ee86f8d1bcd897df2660501bdc70d
Martin Andreev <martin_i_a@abv.bg>
2024-09-13 18:20:15 +0300

    Remove the temporary submit button and add a functionality where every time we change something in the input, we actually make a new request.
    We use useSubmit from react-router-dom for that goal.
    
    We also change the defaultValue values.

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -27,10 +27,10 @@ export const loader = async ({ request }) => {
 const AllJobsContext = createContext();
 
 const AllJobs = () => {
-  const { data } = useLoaderData();
+  const { data, searchValues } = useLoaderData();
 
   return (
-    <AllJobsContext.Provider value={{ data }}>
+    <AllJobsContext.Provider value={{ data, searchValues }}>
       <SearchContainer />
       <JobsContainer />
     </AllJobsContext.Provider>
