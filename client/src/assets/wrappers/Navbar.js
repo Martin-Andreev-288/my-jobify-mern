63cb382a4918359c480e4fcbd1cd724e9e56ef79
Martin Andreev <martin_i_a@abv.bg>
2024-09-14 16:13:05 +0300

    Format pageBtnContainer CSS and edit the all jobs page screenshot
    Format all other CSS files (something a little bit out of the all-jobs-page branch context, but this is a slight exception).

--- client/src/assets/wrappers/Navbar.js
+++ client/src/assets/wrappers/Navbar.js
@@ -1,24 +1,24 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Wrapper = styled.nav`
-height: var(--nav-height);
-display: flex;
-align-items: center;
-justify-content: center;
-box-shadow: 0 1px 0px 0px rgba(0, 0, 0, 0.1);
-background: var(--background-secondary-color);
-.logo {
+  height: var(--nav-height);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 1px 0px 0px rgba(0, 0, 0, 0.1);
+  background: var(--background-secondary-color);
+  .logo {
     display: flex;
     align-items: center;
     justify-content: space-between;
-}
-.nav-center {
+  }
+  .nav-center {
     display: flex;
     width: 90vw;
     align-items: center;
     justify-content: space-between;
-}
-.toggle-btn {
+  }
+  .toggle-btn {
     background: transparent;
     border-color: transparent;
     font-size: 1.75rem;
@@ -26,27 +26,28 @@ background: var(--background-secondary-color);
     cursor: pointer;
     display: flex;
     align-items: center;
-}
-.btn-container {
+  }
+  .btn-container {
     display: flex;
     align-items: center;
-}
-.logo-text {
+  }
+  .logo-text {
     display: none;
-}
-@media (min-width: 992px) {
+  }
+  @media (min-width: 992px) {
     position: sticky;
     top: 0;
 
     .nav-center {
-        width: 90%;
+      width: 90%;
     }
     .logo {
-        display: none;
+      display: none;
     }
     .logo-text {
-        display: block;
+      display: block;
     }
-}
+  }
 `;
+
 export default Wrapper;
